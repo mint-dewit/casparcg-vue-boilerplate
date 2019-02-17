717cbdfa3373ed60e5cf61bb97a1da38b4b13bc4
Balte de Wit <balte.dewit@gmail.com>
2019-02-17 10:41:57 +0100

    feat: initial version

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,46 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <l3d :f0="f0" :f1="f1" ref="l3d" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import './assets/casparcg.css'
+import l3d from './components/l3d.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    l3d
+  },
+  data () {
+    return {
+      f0: "",
+      f1: ""
+    }
+  },
+  mounted () {
+    window.play = () => {
+      this.$refs.l3d.play()
+    }
+    window.stop = () => {
+      this.$refs.l3d.stop()
+    }
+    window.update = data => {
+      if (typeof data === 'string') {
+        data = data.replace(/^(<templateData>|<componentData>|<data>)|(<\/templateData>|<\/componentData>|<\/data>)$/ig, '')
+        try {
+          data = JSON.parse(decodeURI(data))
+        } catch (e) {
+          // data not parsed, most likely it is not json but xml. we don't handle that as of yet.
+        }
+      } 
+      if(data && typeof data === 'object') {
+        if (data.f0 !== undefined) this.f0 = data.f0
+        if (data.f1 !== undefined) this.f1 = data.f1
+      }
+    }
+    window.next = () => {}
   }
 }
 </script>
@@ -21,8 +50,10 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  font-weight: bold;
   color: #2c3e50;
-  margin-top: 60px;
+  position: absolute;
+  width: 100%;
+  height: 100%;
 }
 </style>
---
+++ src/components/l3d.vue
@@ -0,0 +1,123 @@
+<template>
+    <div class="titlesafe left">
+        <div class="block" ref="box">
+            <div class="img"></div>
+        </div>
+        <div class="bars">
+            <div class="bar f0" ref="f0">{{ f0 }}</div>
+            <div class="bar f1" ref="f1">{{ f1 }}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { TimelineLite, Back, Power2, Power4 } from 'gsap'
+
+export default {
+    props: {
+        f0: String,
+        f1: String
+    },
+    data () {
+        return {
+            timelineIntro: null,
+            timelineOutro: null
+        }
+    },
+    methods: {
+        play () {
+            this.timelineIntro.seek(0)
+            this.timelineIntro.play()
+        },
+        stop () {
+            this.timelineOutro.seek(0)
+            this.timelineOutro.play()
+        }
+    },
+    mounted () {
+        const { box, f0, f1 } = this.$refs
+
+        this.timelineIntro = new TimelineLite({ paused: true })
+        this.timelineIntro.to(box, 0.8, {
+            // bottom: '5vh',
+            left: '5vw',
+            // height: '6vw',
+            // width: '6vw',
+            ease: Back.easeOut.config(1.7)
+        })
+        this.timelineIntro.to(f0, 0.4, {
+            marginLeft: 0,
+            opacity: 1,
+            ease: Power2.easeOut
+        }, '-= .2')
+        this.timelineIntro.to(f1, 0.4, {
+            marginLeft: 0,
+            opacity: 1,
+            ease: Power2.easeOut
+        }, '-= .3')
+
+        this.timelineOutro = new TimelineLite({ paused: true })
+        this.timelineOutro.to(f0, 0.4, {
+            marginLeft: '3vw',
+            opacity: 0,
+            ease: Power2.easeIn
+        })
+        this.timelineOutro.to(f1, 0.4, {
+            marginLeft: '3vw',
+            opacity: 0,
+            ease: Power2.easeIn
+        }, '-= .3')
+        this.timelineOutro.to(box, 1, {
+            left: '100vw',
+            ease: Power4.easeIn
+        }, '-= .5')
+    }
+}
+</script>
+
+<style scoped>
+.block {
+    width: 6vw;
+    height: 6vw;
+    background: #dbdbdb;
+    position: fixed;
+    bottom: 5vh;
+    left: -6vw;
+    z-index: 1;
+    padding: 1vw;
+}
+.img {
+    position: relative;
+    height: 100%;
+    width: 100%;
+    background-image: url(../assets/logo.png);
+    background-size: contain;
+    background-repeat: no-repeat;
+}
+
+.bars {
+    position: fixed;
+    height: 6vw;
+    bottom: 5vh;
+    left: 11vw;
+}
+.bar {
+    height: 3vw;
+    position: fixed;
+    font-size: 2.5vh;
+    padding-left: .5vw;
+    padding-right: .5vw;
+    color: white;
+    opacity: 0;
+    margin-left: -3vw;
+}
+.bar.f0 {
+    background: #313131;
+}
+.bar.f1 {
+    background: rgb(168, 168, 168);
+    margin-top: 3vw;
+}
+</style>
+
+
