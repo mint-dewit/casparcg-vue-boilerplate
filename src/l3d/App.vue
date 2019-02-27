6d1a375f0707ea70e1770049582cbc95fc1b59f5
Balte de Wit <balte.dewit@gmail.com>
2019-02-27 08:46:31 +0100

    feat: two name l3d variant
    using the vue sub component architecture this variant reuses animations from the regular l3d but adds a second name/title. the parent component automatically selects the right l3d component.

--- src/l3d/App.vue
+++ src/l3d/App.vue
@@ -1,22 +1,26 @@
 <template>
   <div id="app" class="fullframe">
-    <l3d :f0="f0" :f1="f1" ref="l3d" />
+    <l3d-two-name v-if="f2 || f3"  :f0="f0" :f1="f1" :f2="f2" :f3="f3" ref="l3d" />
+    <l3d v-else :f0="f0" :f1="f1" ref="l3d" />
   </div>
 </template>
 
 <script>
 import '../assets/casparcg.css'
 import l3d from './components/l3d.vue'
+import l3dTwoName from './components/l3dTwoName.vue'
 
 export default {
   name: 'app',
   components: {
-    l3d
+    l3d, l3dTwoName
   },
   data () {
     return {
-      f0: "",
-      f1: ""
+      f0: undefined,
+      f1: undefined,
+      f2: undefined,
+      f3: undefined
     }
   },
   mounted () {
@@ -36,8 +40,10 @@ export default {
         }
       } 
       if(data && typeof data === 'object') {
-        if (data.f0 !== undefined) this.f0 = data.f0
-        if (data.f1 !== undefined) this.f1 = data.f1
+        this.f0 = data.f0
+        this.f1 = data.f1
+        this.f2 = data.f2
+        this.f3 = data.f3
       }
     }
     window.next = () => {}
---
+++ src/l3d/components/l3dTwoName.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="titlesafe left">
+        <div class="block">
+            <div class="inner-block" ref="box">
+                <div class="img"></div>
+            </div>
+        </div>
+        <div class="bars">
+            <div class="bar f0">
+                <div class="inner" ref="f0">
+                    {{ f0 }}
+                    <span class="right-align">{{ f2 }}</span>
+                </div>
+            </div>
+            <div class="bar f1">
+                <div class="inner" ref="f1">
+                    {{ f1 }}
+                    <span class="right-align">{{ f3 }}</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import l3d from './l3d'
+
+export default {
+    extends: l3d,
+    props: {
+        f2: String,
+        f3: String
+    }
+}
+</script>
+
+<style scoped>
+.bar .inner {
+    width: 84vw;
+}
+.right-align {
+    text-align: right;
+    float: right;
+}
+</style>
+
+
